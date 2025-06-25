67f505849e682f829d7c47fffa28a5333ad81549
Chris Funes <chrisfunes88@gmail.com>
2025-06-25 12:09:26 -0400

    Finalize working demo version for public release

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  base: "./", // 👈 ¡ESTA LÍNEA ES CLAVE PARA VERCEL!
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
